dc8cc5b66ed07420795d49b7f596b28c69d4b351
Positron Games <sharrvathgopi@gmail.com>
2025-06-12 20:27:10 +0530

    Game class and Window Init

--- TwoPlayerBattleGame/Game.cs
+++ TwoPlayerBattleGame/Game.cs
@@ -1,6 +1,62 @@
+using Raylib_cs;
+
 namespace TwoPlayerBattleGame;
+
+using static Raylib_cs.Raylib;
 
 public class Game
 {
+
+    private int SCREEN_W;
+    private int SCREEN_H;
+
+    public bool running;
     
+    
+    public Game(int screen_w, int screen_h)
+    {
+        running = true;
+        
+        SCREEN_W = screen_w;
+        SCREEN_H = screen_h;
+        
+        InitWindow(SCREEN_W, SCREEN_H, "Game");
+        SetTargetFPS(60);
+        
+        
+    }
+
+    public void Update(float dt)
+    {
+        if (WindowShouldClose() && running)
+            running = false;
+    }
+
+    public void Draw()
+    {
+        BeginDrawing();
+        
+        ClearBackground(Color.Green);
+        
+        EndDrawing();
+    }
+
+    public void Run(float dt)
+    {
+        if (running)
+        {
+            Update(dt);
+            Draw();
+            
+        }
+        else if (!running)
+        {
+            Terminate();
+        }
+    }
+
+    public void Terminate()
+    {
+        CloseWindow();
+    }
 }
--- TwoPlayerBattleGame/Program.cs
+++ TwoPlayerBattleGame/Program.cs
@@ -1,9 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Raylib_cs;
+using TwoPlayerBattleGame;
+
 class Program
 {
     static void Main(string[] args)
     {
-        
+        Game game = new Game(1280, 720);
+        while (game.running)
+        {
+            float dt = Raylib.GetFrameTime();
+            game.Run(dt);
+        }
     }
 }
