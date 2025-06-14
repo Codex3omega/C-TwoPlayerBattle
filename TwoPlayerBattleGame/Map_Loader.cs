524ebdbdd170c440ff09c1e60552d3b56e53dd69
Positron Games <sharrvathgopi@gmail.com>
2025-06-14 16:44:36 +0530

    Beginnings of a map Loader

--- TwoPlayerBattleGame/Map_Loader.cs
+++ TwoPlayerBattleGame/Map_Loader.cs
@@ -1,21 +1,33 @@
 using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
 namespace TwoPlayerBattleGame;
 
-public class Map_Loader
+public class Map_Manager
 {
-    public Map_Loader(ref int[,] envsList)
+    private int cell_size = 16;
+    private int screen_width = 1920;
+    private int screen_height = 1080;
+
+    private Texture2D atlas;
+    
+    private List<List<int>> envsList;
+        
+    
+    public Map_Manager()
     {
         try
         {
             using (StreamReader reader = new StreamReader("Environment.txt"))
             {
-                if (!reader.EndOfStream)
+                int x = 0;
+                int y = 0;
+                int data;
+                while ((data = reader.Read()) != -1 && x < screen_width/cell_size &&  y < screen_height/cell_size)
                 {
-                    int x = 0;
-                    int y = 0;
-                   // while(x < envsList.)
-
-
+                    envsList[x][y] = data;
+                    x += 1;
+                    y += 1;
                 }
             }
         }
@@ -23,6 +35,21 @@ public class Map_Loader
         {
             Console.WriteLine("File Could Not Be Loaded");
             Console.WriteLine(e);
+        }
+    }
+
+    public void Draw()
+    {
+        int cell_x = 0;
+        int cell_y = 0;
+        
+        foreach (var x in envsList)
+        {
+            foreach (var y in envsList)
+            {
+                
+               // DrawTexturePro(atlas, new Rectangle(y*cell_size, y*cell_size,cell_size, cell_size ));
+            }
         }
     }
     
